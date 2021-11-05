a6abafaa7b7ed372ebbbdc67c20280a11fa1a2e3
zackhamin <ishaqhamin@gmail.com>
2021-11-05 14:04:55 +0000

    Explored more value types

--- src/main/scala/variablesValues/ValuesVariablesTypes.scala
+++ src/main/scala/variablesValues/ValuesVariablesTypes.scala
@@ -2,4 +2,24 @@ package variablesValues
 
 object ValuesVariablesTypes extends App{
 
+  //Value - val are immutable.
+  //Type can be inferred does not need declaring val x: Int = 42
+  val x = 42;
+  println(x)
+
+  // No
+ // x = 2
+
+  val aString: String = " Hello";
+  val aBoolean: Boolean = false
+  val aChar: Char = 'A'
+  val aShort: Short = 4565
+  val aLong: Long = 78787878789845L
+  val aFloat: Float = 2.15f
+  val aDouble: Double = 2.35
+
+  //Variable is mutable
+  var aVariable: Int = 15
+  aVariable = 5
+
 }
